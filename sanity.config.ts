d339bb679a3496cdf7dbdf301ca80b705f90974f
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-04-19 11:29:27 +0200

    First commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,7 +8,7 @@ const devOnlyPlugins = [getStartedPlugin()]
 
 export default defineConfig({
   name: 'default',
-  title: 'snow-walrus',
+  title: 'Grøn Blok',
 
   projectId: 'at6buhh6',
   dataset: 'production',
@@ -19,4 +19,3 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
-
