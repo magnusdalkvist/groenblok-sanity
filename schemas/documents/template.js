7c0327bf9542f4218161ed268b4b6222d4e360d2
Markus <mark394e@stud.kea.dk>
2023-05-08 21:16:21 +0200

    quick fix

--- schemas/documents/template.js
+++ schemas/documents/template.js
@@ -34,7 +34,7 @@ export default defineField({
         {type: 'module.latestArticles'},
         {type: 'module.imageGallery'},
         {type: 'module.shopSection'},
-        {type: 'module.numberedeInfoList'},
+        {type: 'module.numberedInfoList'},
         {type: 'module.header'},
         {type: 'module.footer'},
       ],
